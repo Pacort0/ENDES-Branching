0272db8248bf2398fd38add9bb879e3fd8fe580e
lcardozo <lcardozo@106-25.info2>
2023-01-12 12:59:58 +0100

    Verificacion de primos Rama Luisa

--- ENDES-Branching/src/paquete/clasemain.java
+++ ENDES-Branching/src/paquete/clasemain.java
@@ -15,7 +15,24 @@ public class clasemain {
 		b = sc.nextInt();
 		
 		System.out.println("El valor de 'a' es " + a + " y el de 'b' " + b);
+		
+		System.out.println("El valor de a es primo? : "+clasemain.sumaDeNumerosEsPrimo(a));
+		System.out.println("El valor de b es primo? : "+ clasemain.sumaDeNumerosEsPrimo(b));
+		
 		sc.close();
+	    }
+	public static boolean sumaDeNumerosEsPrimo(int x) {
+		
+		boolean primo = true; //declaramos este boolean llamado primo que equivale a true
+		
+		for (int i = 2; i<x; i++) {
+			
+			//En cada vuelta del for va a dividir el numero introducido entre i, si en algun caso resulta ser 0 pues el numero no es primo
+			if (x % i == 0) {
+				//entonces nuestro boolean pasa a ser false
+				primo = false;
+			}
+		}
+	return primo;//solicitamos que devuelva primo
 	}
-
 }
